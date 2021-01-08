4f8e3b9dd64c6f330b75490c2ea87fc0b1441331
POPlol333 <adampoplol@gmail.com>
2021-01-08 12:15:03 +0100

    Init

--- src/fr/cardi/mainPackage/listeners/PlayerRelative.java
+++ src/fr/cardi/mainPackage/listeners/PlayerRelative.java
@@ -1,11 +1,15 @@
 package fr.cardi.mainPackage.listeners;
 
-import org.bukkit.ChatColor;
-import org.bukkit.Material;
+import org.bukkit.*;
+import org.bukkit.entity.EnderCrystal;
+import org.bukkit.entity.EnderDragon;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerChangedWorldEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.inventory.ItemStack;
@@ -17,7 +21,7 @@ import java.util.List;
 
 public class PlayerRelative implements Listener {
 
-
+    Server server = Bukkit.getServer();
 
     @EventHandler
     public void onJoin(PlayerJoinEvent e)
@@ -43,7 +47,7 @@ public class PlayerRelative implements Listener {
 
             Player tueur = victim.getKiller();
 
-            e.setDeathMessage(String.format("§8§l[§6§lBroadcast§8§l] §r%s a tué %s", tueur.getName(), victim.getName()));
+            e.setDeathMessage(String.format("§8§l[§6§lBroadcast§8§l] §4§l%s §ra tué §c%s", tueur.getName(), victim.getName()));
 
             ItemStack head = new ItemStack(Material.SKULL_ITEM, 1, (byte) 3);
             SkullMeta meta = (SkullMeta) head.getItemMeta();
@@ -60,7 +64,56 @@ public class PlayerRelative implements Listener {
 
     }
 
+    @EventHandler
+    public void onChangeWorld(PlayerChangedWorldEvent e) {
+
+        Player player = e.getPlayer();
+
+        World world = e.getPlayer().getWorld();
+
+        if(world.getEnvironment() == World.Environment.THE_END) {
+            System.out.println(String.format(ChatColor.GOLD+"%s EST ENTRÉ DANS L'END", player.getName()));
+
+        } else if (world.getEnvironment() == World.Environment.NETHER ){
+            System.out.println(String.format(ChatColor.GOLD+"%s EST ENTRÉ DANS LE NETHER", player.getName()));
+        } else if (world.getEnvironment() == World.Environment.NORMAL ) {
+            System.out.println(String.format(ChatColor.GOLD+"%s EST ENTRÉ DANS L'OVERWORLD", player.getName()));
+        }
+    }
 
 
+    static ArrayList listParticipantDragon = new ArrayList();
+
+    @EventHandler
+    public void DragonPlayerParticipant(EntityDamageByEntityEvent e){
+
+        if(e.getEntity() instanceof EnderDragon || e.getEntity() instanceof EnderCrystal){
+
+            if(e.getDamager() instanceof Player) {
+                if(listParticipantDragon.contains(e.getDamager().getName())) {
+                    System.out.println(String.format("%s est déjà dans listParticipantDragon", e.getDamager().getName()));
+                } else {
+                    listParticipantDragon.add(e.getDamager().getName());
+                    System.out.println(String.format("%s a rejoint listParticipantDragon", e.getDamager().getName()));
+                }
+            }
+        }
+    }
+
+    @EventHandler
+    public void DragonDeath(EntityDeathEvent e) {
+
+        if(e.getEntity() instanceof EnderDragon) {
+
+            server.dispatchCommand(server.getConsoleSender(), "broadcast §5§lLe dragon est mort, voici la liste des participants :");
+
+            for(int i = 0; i < listParticipantDragon.size(); i++) {
+                server.dispatchCommand(server.getConsoleSender(), String.format("broadcast §5§l-%s", listParticipantDragon.get(i)));
+                System.out.println("Indice de rang "+ i + " " +listParticipantDragon.get(i));
+            }
+            listParticipantDragon.clear();
+            System.out.println("La liste listParticipantDragon a bien été reset !");
+        }
+    }
 
 }
